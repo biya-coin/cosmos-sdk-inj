0e54211e3aa4ced91a13adc0b433c956c84f1552
Dusan Brajovic <dbrajovic3@gmail.com>
2026-03-17 19:46:03 +0100

    impl: add eip712v2 as a supported type

--- x/auth/signing/verify.go
+++ x/auth/signing/verify.go
@@ -52,6 +52,8 @@ func internalSignModeToAPI(mode signing.SignMode) (signingv1beta1.SignMode, erro
 		return signingv1beta1.SignMode_SIGN_MODE_TEXTUAL, nil
 	case signing.SignMode_SIGN_MODE_DIRECT_AUX:
 		return signingv1beta1.SignMode_SIGN_MODE_DIRECT_AUX, nil
+	case signing.SignMode_SIGN_MODE_EIP712_V2:
+		return signingv1beta1.SignMode_SIGN_MODE_EIP712_V2, nil
 	default:
 		return signingv1beta1.SignMode_SIGN_MODE_UNSPECIFIED, fmt.Errorf("unsupported sign mode %s", mode)
 	}
